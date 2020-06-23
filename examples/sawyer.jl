b927d7edca5337b52f20f7a5fcf567718c20c72e
janbruedigam <jan.bruedigam@t-online.de>
2020-06-23 17:52:48 +0200

    Add sawyer simulation

---
+++ examples/sawyer.jl
@@ -0,0 +1,9 @@
+using ConstrainedDynamics
+using ConstrainedDynamicsVis
+
+
+path = "examples/examples_files/sawyer_simple.urdf"
+mech, shapes = Mechanism(path, floating=false, g = -.5)
+
+storage = simulate!(mech, 10., record = true)
+visualize(mech, storage, shapes)
