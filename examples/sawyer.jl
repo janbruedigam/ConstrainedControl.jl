6618f0789f8d4e8079c4ef34b667940d5382d314
janbruedigam <jan.bruedigam@t-online.de>
2020-06-25 17:39:48 +0200

    Simplify sawyer example

--- examples/sawyer.jl
+++ examples/sawyer.jl
@@ -7,36 +7,26 @@ using LinearAlgebra
 path = "examples/examples_files/sawyer_arm.urdf"
 mech, shapes = Mechanism(path, floating=false, g = -0.0)
 
-setPosition!(mech,mech.eqconstraints[8],[0.0])
-setPosition!(mech,mech.eqconstraints[9],[-pi/2])
-setPosition!(mech,mech.eqconstraints[10],[-pi/2])
-setPosition!(mech,mech.eqconstraints[11],[pi/2])
-setPosition!(mech,mech.eqconstraints[12],[0.0])
-setPosition!(mech,mech.eqconstraints[13],[0.0])
-setPosition!(mech,mech.eqconstraints[14],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j0"],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j1"],[-pi/2])
+setPosition!(mech,mech.eqconstraints["right_j2"],[-pi/2])
+setPosition!(mech,mech.eqconstraints["right_j3"],[pi/2])
 
 xd=[mech.bodies[i].state.xc for i=1:length(mech.bodies)]
 qd=[mech.bodies[i].state.qc for i=1:length(mech.bodies)]
 
-setPosition!(mech,mech.eqconstraints[8],[0.0])
-setPosition!(mech,mech.eqconstraints[9],[0.0])
-setPosition!(mech,mech.eqconstraints[10],[0.0])
-setPosition!(mech,mech.eqconstraints[11],[0.0])
-setPosition!(mech,mech.eqconstraints[12],[0.0])
-setPosition!(mech,mech.eqconstraints[13],[0.0])
-setPosition!(mech,mech.eqconstraints[14],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j0"],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j1"],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j2"],[0.0])
+setPosition!(mech,mech.eqconstraints["right_j3"],[0.0])
 
 
 Q = [diagm(ones(12))*1000.0 for i=1:7]
 R = [ones(1,1) for i=1:7]
 
-for (id,_) in pairs(mech.eqconstraints)
-    deactivate!(mech,id)
-end
+ConstrainedDynamics.deactivateConstraints!(mech)
 lqr = LQR(mech, getid.(mech.bodies), getid.(mech.eqconstraints), Q, R, 20., xd=xd, qd=qd)
-for (id,_) in pairs(mech.eqconstraints)
-    activate!(mech,id)
-end
+ConstrainedDynamics.activateConstraints!(mech)
 
 
 storage = simulate!(mech,20.,lqr,record = true)
